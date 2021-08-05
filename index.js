8ead5339c5aa81e0c074b98d4cabce7390f21400
eranilkapoor <kapoor.anil749@gmail.com>
2021-08-05 22:58:23 +0530

    Changes

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ app.get('/',(req, res)=>{
 	res.send('Hello world!');
 });
 
+app.get('/api/call', async function (req, res){
+	try {
+	    res.send(await search('Anil'))
+	} catch (e){
+	    console.log(e);
+	}
+});
+
+async function search(data) {
+	let result = new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			console.log('Anil Kapoor');
+			resolve();
+		}, 60000);
+	});
+
+	return await result;
+}
+
 app.listen(3000,()=>{
 	console.log('Server running on port 3000');
 });
